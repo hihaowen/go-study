76889fd8ae0dd37f46a5f19f42dccf4b7d779649
haowen.hi <haowen.hi@gmail.com>
2020-04-05 03:51:12 +0800

    Fix ts file cache

--- best-practices/grabZuo9/main.go
+++ best-practices/grabZuo9/main.go
@@ -198,7 +198,7 @@ func newRetryLock() *retryLock {
 func grabIntoFile(url, toFile string) string {
 	// if file exists
 	_, err := os.Stat(toFile)
-	if err != nil {
+	if err == nil {
 		log.Printf("grab from local cache: %s\n", toFile)
 		return toFile
 	}
